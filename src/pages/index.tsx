9536abaf780e5ea348b41227b3edbb4a0964c6f3
kota5140 <xkota1119x@gmail.com>
2024-04-30 02:47:21 +0900

    deploy problem fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import { createTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 const Home: NextPage = () => {
   const [accessToken, setAccessToken] = useState<string>(""); // ログイン状態を管理
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
         },
       },
     },
-  });
+  } as any);
 
   useEffect(() => {
     const storedToken =
@@ -67,29 +67,31 @@ const Home: NextPage = () => {
   if (isLoading) return <CircularProgress sx={{ flex: 1 }} />;
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Head>
-        <title>To Do App</title>
-        <meta
-          name="description"
-          content="The easiest way to track your daily work"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100vh",
-          overflowY: "auto",
-        }}
-      >
-        <Header token={accessToken} onLogout={handleLogout} />
-        {!isLogin && <Login onLogin={handleLogin} />}
-        {isLogin && <Mypage token={accessToken} />}
-      </Box>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Head>
+          <title>To Do App</title>
+          <meta
+            name="description"
+            content="The easiest way to track your daily work"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            overflowY: "auto",
+          }}
+        >
+          <Header token={accessToken} onLogout={handleLogout} />
+          {!isLogin && <Login onLogin={handleLogin} />}
+          {isLogin && <Mypage token={accessToken} />}
+        </Box>
+      </ThemeProvider>
+    </>
   );
 };
 
